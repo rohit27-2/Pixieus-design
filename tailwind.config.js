fbd899ed71198adc91cae5ea74e6d0093f6c7cd1
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-07-09 23:13:31 +0530

    New Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+     }
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+}
+
