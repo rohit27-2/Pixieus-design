2fa46485d7bc48d933b57bb4384c6511657a7bbf
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-07-12 00:03:49 +0530

    Updated css config fiels

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js",'./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins'],
-     }
+        poppins: ["Poppins"],
+      },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
